93e6e7451a648462f5d541c3ee1d1144f1ebdd02
Ifeanyi-design <ifeanyiagada123@gmail.com>
2026-02-19 16:13:24 +0100

    Add monetag zone environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -80,6 +80,9 @@ def create_app(config_class=Config):
                 "sticky_desktop": sticky_desktop,
                 "sticky_mobile": sticky_mobile,
                 "pop_url": pop_url,
+                "monetag_inpage_zone": os.getenv("MONETAG_INPAGE_ZONE"),
+                "monetag_vignette_zone": os.getenv("MONETAG_VIGNETTE_ZONE"),
+                "monetag_push_zone": os.getenv("MONETAG_PUSH_ZONE"),
             }
         )
 
